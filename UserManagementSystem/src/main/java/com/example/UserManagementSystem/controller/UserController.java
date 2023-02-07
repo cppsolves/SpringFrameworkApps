678f02aaf7ed4d53da76cf78c72fd868dbf8e2c8
cppSolves <nairanshul.9790@gmail.com>
2023-02-07 13:58:38 +0530

    User Management System

---
+++ UserManagementSystem/src/main/java/com/example/UserManagementSystem/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.example.UserManagementSystem.controller;
+
+import com.example.UserManagementSystem.model.User;
+import com.example.UserManagementSystem.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("masterUser")
+public class UserController {
+    @Autowired
+    private UserService userservice;
+    @PostMapping ("adduser")
+    public ResponseEntity<User> add(@RequestBody User obj){
+        userservice.adduser(obj);
+        return new ResponseEntity<User>(HttpStatus.OK);
+    }
+    @GetMapping("getUser/{id}")
+    public User addid(@PathVariable int id){
+        return userservice.getuser(id);
+    }
+    @GetMapping("getAllUser")
+    public List<User> getall(){
+        return userservice.getalluser();
+    }
+    @PutMapping("updateUserInfo/{id}")
+    public ResponseEntity<User> update(@PathVariable int id,@RequestBody User obj){
+        User Obj = userservice.update(id,obj);
+        if(null!=Obj){
+            return new ResponseEntity<User>(HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<User>(HttpStatus.BAD_REQUEST);
+        }
+    }
+    @DeleteMapping("deleteUser/{id}")
+    public void delete(@PathVariable int id){
+        userservice.delete(id);
+    }
+}
---
+++ UserManagementSystem/src/main/java/com/example/UserManagementSystem/service/UserService.java
@@ -0,0 +1,44 @@
+package com.example.UserManagementSystem.service;
+
+import com.example.UserManagementSystem.model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+    private final List<User> list_;
+    public UserService(){
+        list_ = new ArrayList<>();
+    }
+    public void adduser(User obj){
+        list_.add(obj);
+    }
+    public User getuser(int id){
+        int low=0,high=list_.size()-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(list_.get(mid).getUserId() == id) return list_.get(mid);
+            else if(list_.get(mid).getUserId()>id) high = mid-1;
+            else low=mid+1;
+        }
+        return new User(0,"notfound","notfound","notfound","notfound");
+    }
+    public List<User> getalluser(){
+        return list_;
+    }
+    public User update(int id,User obj){
+        if(id > list_.size()-1) return null;
+        list_.get(id).setName(obj.getName());
+        list_.get(id).setUserName(obj.getUserName());
+        list_.get(id).setAddress(obj.getAddress());
+        list_.get(id).setNumber(obj.getNumber());
+        return list_.get(id);
+    }
+    public void delete(int id){
+        list_.remove(id);
+    }
+
+
+}
